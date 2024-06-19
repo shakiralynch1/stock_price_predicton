01c28ae40e9feab23daae2799255177601a6a2da
shakiralynch1 <d00241906@student.dkit.ie>
2024-06-19 20:22:11 +0100

    :chore: Update main.py to read data from Google_Stock_Price_Test.csv and print the values

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-%matplotlib inline
+
 
 # Load the data
-data = pd.read_csv('Google_Stock_Price_Train.csv')
+data = pd.read_csv('Google_Stock_Price_Test.csv')
+data = data.iloc[:, 1:2].values
+print(data)
